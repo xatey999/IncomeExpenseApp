34fb9c840cd3d3e9dc47188e8b94546400c743d3
Ujjwal Adhikari <pokhreli.thito10@gmail.com>
2024-07-15 13:29:53 +0545

    initialize the laravel files and created model and migrations for transactions

---
+++ app/Models/TransactionModel.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class TransactionModel extends Model
+{
+    use HasFactory;
+
+    protected $table = 'transactions';
+
+    protected $fillable = [
+        'description',
+        'amount',
+        'income',
+        'expense',
+        'transaction_date',
+    ];
+}
